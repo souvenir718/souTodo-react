af8aff4a089846586b1fa07db426ed4e19717c0b
souvenir718 <mongkey0@naver.com>
2021-02-19 18:56:42 +0900

    todo test data

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -3,6 +3,27 @@ import React from 'react';
 const todos = [
     {
         id: 1,
+        title: 'todo1',
+        isDone: false,
+        date: '2021.01.09',
+    },
+    {
+        id: 2,
+        title: 'todo2',
+        isDone: false,
+        date: '2021.01.09',
+    },
+    {
+        id: 3,
+        title: 'todo3',
+        isDone: false,
+        date: '2021.01.09',
+    },
+    {
+        id: 4,
+        title: 'todo4',
+        isDone: false,
+        date: '2021.01.09',
     },
 ];
 
